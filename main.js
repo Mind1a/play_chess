c3f4528a7b0232e00e766491d22d9d92cd21e450
zikniki <102323125+zikniki@users.noreply.github.com>
2024-02-23 05:25:34 +0400

    feature/header-responsive
    adding header responsive
    adding popup

--- main.js
+++ main.js
@@ -48,3 +48,65 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// ----------popup----------
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenuButton = document.querySelector(".burger-menu button");
+  const popupContainer = document.querySelector(
+    ".burger-menu .popup-conteiner"
+  );
+  const closeButton = document.querySelector(".burger-menu .close_button");
+  function togglePopup() {
+    if (
+      popupContainer.style.display === "none" ||
+      popupContainer.style.display === ""
+    ) {
+      popupContainer.style.display = "flex";
+      burgerMenuButton.style.display = "none";
+    } else {
+      popupContainer.style.display = "none";
+      burgerMenuButton.style.display = "block";
+    }
+  }
+
+  burgerMenuButton.addEventListener("click", togglePopup);
+
+  closeButton.addEventListener("click", function () {
+    popupContainer.style.display = "none";
+    burgerMenuButton.style.display = "block";
+  });
+});
+
+// ----------popup navbar-hovers-----------
+document.addEventListener("DOMContentLoaded", function () {
+  const navButtons = document.querySelectorAll(".navbtn-popup");
+  navButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      navButtons.forEach((btn) => {
+        btn.classList.remove("active-popup");
+      });
+      this.classList.add("active-popup");
+    });
+  });
+});
+
+function popupgoToPage2() {
+  var page1 = document.getElementById("page1");
+  var page2 = document.getElementById("page2");
+  var video = document.getElementById("video1");
+
+  page1.style.display = "none";
+  page2.style.display = "block";
+}
+
+// Function to switch to page 1
+function popupgoToPage1() {
+  var page1 = document.getElementById("page1");
+  var page2 = document.getElementById("page2");
+  var video = document.getElementById("video1");
+
+  page2.style.display = "none";
+  page1.style.display = "block";
+
+  video.pause();
+}
