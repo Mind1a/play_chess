537e7b059338520da3a55117dacdf748ca1d7006
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2024-03-16 18:12:08 +0400

    comit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // -------swiper-slider-----
-const swiper_for_img = new Swiper(".swiper-for-img", {
+const swiper_for_img = new Swiper('.swiper-for-img', {
   speed: 400,
   spaceBetween: 10,
   slidesPerView: 1,
@@ -7,16 +7,16 @@ const swiper_for_img = new Swiper(".swiper-for-img", {
   loop: true,
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
 // -------swiper-workers-card-slider--------
-const swiper = new Swiper(".swiper-workers-section", {
+const swiper = new Swiper('.swiper-workers-section', {
   speed: 400,
   spaceBetween: 10,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
 
   on: {
     reachEnd: function () {
@@ -41,150 +41,73 @@ const swiper = new Swiper(".swiper-workers-section", {
   },
 });
 
-// ----------navbar-pages-------
-function goToPage2() {
-  let page1 = document.getElementById("page1");
-  let page2 = document.getElementById("page2");
-  let video = document.getElementById("video1");
+// Global variable to keep track of the active page
+let activePage = 1;
 
-  page1.style.display = "none";
-  page2.style.display = "block";
-}
+// Function to update active class
+function updateActiveClass() {
+  const navButtons = document.querySelectorAll('.navbtn');
+  const navPopupButtons = document.querySelectorAll('.navbtn-popup');
 
-// Function to switch to page 1
-function goToPage1() {
-  let page1 = document.getElementById("page1");
-  let page2 = document.getElementById("page2");
-  let video = document.getElementById("video1");
-
-  page2.style.display = "none";
-  page1.style.display = "block";
-
-  video.pause();
-}
-
-// ----------navbar-hovers------
-
-document.addEventListener("DOMContentLoaded", function () {
-  const navButtons = document.querySelectorAll(".navbtn");
-  navButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      navButtons.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      this.classList.add("active");
-    });
+  // Update nav buttons
+  navButtons.forEach((button, index) => {
+    button.classList.toggle('active', index === activePage - 1);
   });
-});
 
-// ----------popup----------
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenuButton = document.querySelector(".burger-menu button");
-  const popupContainer = document.querySelector(
-    ".burger-menu .popup-conteiner"
-  );
-  const closeButton = document.querySelector(".burger-menu .close_button");
-  function togglePopup() {
-    if (
-      popupContainer.style.display === "none" ||
-      popupContainer.style.display === ""
-    ) {
-      popupContainer.style.display = "flex";
-      burgerMenuButton.style.display = "none";
-    } else {
-      popupContainer.style.display = "none";
-      burgerMenuButton.style.display = "block";
-    }
-  }
-
-  burgerMenuButton.addEventListener("click", togglePopup);
-
-  closeButton.addEventListener("click", function () {
-    popupContainer.style.display = "none";
-    burgerMenuButton.style.display = "block";
+  // Update nav popup buttons
+  navPopupButtons.forEach((button, index) => {
+    button.classList.toggle('active-popup', index === activePage - 1);
   });
+}
+
+// Function to switch pages
+function switchPage(pageNumber) {
+  const page1 = document.getElementById('page1');
+  const page2 = document.getElementById('page2');
+  const video = document.getElementById('video1');
+
+  if (pageNumber === 1) {
+    page2.style.display = 'none';
+    page1.style.display = 'block';
+    video.pause();
+  } else if (pageNumber === 2) {
+    page1.style.display = 'none';
+    page2.style.display = 'block';
+  }
+
+  activePage = pageNumber;
+  updateActiveClass();
+}
+
+// Function to handle burger menu button click event
+function handleBurgerMenuClick() {
+  const burgerMenu = document.querySelector('.popup-conteiner');
+  const overlay = document.querySelector('.overlay');
+  burgerMenu.style.display = 'flex';
+  overlay.style.display = 'block';
+}
+
+// Function to handle close button click event
+function handleCloseButtonClick() {
+  const burgerMenu = document.querySelector('.popup-conteiner');
+  const overlay = document.querySelector('.overlay');
+  burgerMenu.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+// Add event listeners
+document
+  .querySelector('#menuIcon')
+  .addEventListener('click', handleBurgerMenuClick);
+document
+  .querySelector('.close_button')
+  .addEventListener('click', handleCloseButtonClick);
+window.addEventListener('resize', updateActiveClass);
+
+// Add event listeners for page navigation
+document.querySelectorAll('.navbtn').forEach((button, index) => {
+  button.addEventListener('click', () => switchPage(index + 1));
 });
-
-// ----------popup navbar-hovers-----------
-document.addEventListener("DOMContentLoaded", function () {
-  const navButtons = document.querySelectorAll(".navbtn-popup");
-  navButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      navButtons.forEach((btn) => {
-        btn.classList.remove("active-popup");
-      });
-      this.classList.add("active-popup");
-    });
-  });
+document.querySelectorAll('.navbtn-popup').forEach((button, index) => {
+  button.addEventListener('click', () => switchPage(index + 1));
 });
-
-function popupgoToPage2() {
-  let page1 = document.getElementById("page1");
-  let page2 = document.getElementById("page2");
-  let video = document.getElementById("video1");
-
-  page1.style.display = "none";
-  page2.style.display = "block";
-}
-
-// Function to switch to page 1
-function popupgoToPage1() {
-  let page1 = document.getElementById("page1");
-  let page2 = document.getElementById("page2");
-  let video = document.getElementById("video1");
-
-  page2.style.display = "none";
-  page1.style.display = "block";
-
-  video.pause();
-}
-
-// ---------- see more -----------
-function resetText() {
-  const secondPart = document.getElementById("second-part");
-  const button = document.querySelector(".see-more button");
-
-  secondPart.classList.add("hidden");
-  button.textContent = "ვრცლად";
-}
-
-window.addEventListener("resize", function () {
-  if (window.innerWidth >= 481) {
-    resetText();
-  }
-});
-document.addEventListener("DOMContentLoaded", function () {
-  resetText();
-});
-
-// ---------------------------
-function toggleText() {
-  const secondPart = document.getElementById("second-part");
-  const button = document.querySelector(".see-more button");
-
-  if (secondPart.classList.contains("hidden")) {
-    secondPart.classList.remove("hidden");
-    button.textContent = "აკეცვა";
-  } else {
-    secondPart.classList.add("hidden");
-    button.textContent = "ვრცლად";
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
-}
-
-//---------------------detect-OS----------------
-class DownloadMessage {
-  constructor() {
-    this.messageElement = document.getElementById("download-message");
-  }
-
-  isWindows() {
-    return /win/i.test(navigator.platform);
-  }
-
-  displayMessage() {
-    if (!this.isWindows()) {
-      this.messageElement.style.display = "block";
-    }
-  }
-}
